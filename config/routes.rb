97a718fcfc9ba9082cd35ba9fc25634c92ba5412
Joyce Kao <joyceykao@gmail.com>
2020-04-20 13:35:16 +0200

    corrected root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: cocktails
+  root to: 'cocktails#index'
   resources :cocktails, only: [:index, :new, :show, :create] do
     resources :doses, only: [:new, :create]
   end
